cd87bf23aa911e4dff2404e260ecd567459604d9
Jane Zhang <jiejanezhang2012@gmail.com>
2023-06-16 10:28:05 +1200

    working on form

--- client/components/Form.tsx
+++ client/components/Form.tsx
@@ -1,7 +1,41 @@
-function Form() {
-  return (
+import { useIsRestoring } from "@tanstack/react-query"
+import { options } from "superagent"
 
-    <p>I'm the Form</p>
+function Form() {
+
+  function handleChange() {
+
+  }
+
+  function handleSelect() {
+
+  }
+
+  function handleSubmit() {
+
+  }
+
+  return (
+    <>
+      <form action="">
+        <div>
+          <label htmlFor="name">Player Name: </label>
+          <input type="text" name="name" id="" onChange={handleChange} value={userName} />
+        </div>
+        <div>
+          <label htmlFor="class">Player Class</label>
+          <select name="class" id="" onSelect={handleSelect} value={playerClassId}>
+            <option value="">Select your class</option>
+            {playerClasess.map((playerClass) =>
+              <option key={playerClassId} value={playerClassId}>{playerClass.name}</option>
+            )}
+          </select>
+        </div>
+        <div>
+          <button type="submit" onSubmit={handleSubmit}>Submit</button>
+        </div>
+      </form>
+    </>
 
   )
 }
