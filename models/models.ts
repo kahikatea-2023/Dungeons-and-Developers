3ad02e72f1bba3e666795a75a2a1daefe2d097cc
Andrew <andrewjtrotter@gmail.com>
2023-06-15 15:17:55 +1200

    completed models for data contract

---
+++ models/models.ts
@@ -0,0 +1,21 @@
+export interface OutcomeDraft {
+  outcome: string
+}
+
+export interface Outcome extends OutcomeDraft {
+  id: number
+}
+
+export interface Class {
+  id: number
+  className: string
+}
+
+export interface UserDraft {
+  name: string
+  classId: number
+}
+
+export interface User extends UserDraft {
+  id: number  
+}
