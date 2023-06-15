7870bc4825053a03e6252326b76f5be549a151e4
Andrew <andrewjtrotter@gmail.com>
2023-06-15 17:16:43 +1200

    rendered Game and Form components

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -5,11 +5,8 @@ import Form from './Form'
 import Game from './Game'
 
 function App() {
-  const outcomes = useAppSelector((state) => state.outcomes)
-  const dispatch = useAppDispatch()
-  useEffect(() => {
-    dispatch(fetchOutcomes())
-  }, [dispatch])
+ 
+  
 
   return (
     <main>
--- client/components/Game.tsx
+++ client/components/Game.tsx
@@ -1,9 +1,17 @@
+import { getOutcomes } from "../apis/api"
+
 function Game() {
   
+ // const outcomes = useAppSelector((state) => state.outcomes)
+  // const dispatch = useAppDispatch()
 
+  const { isLoading, data } = useQuery('getOutcomes', async () => {
+    return await getOutcomes()
+  })
   return(
 
     <p>I'm the game</p>
+    
   )
 }
 
