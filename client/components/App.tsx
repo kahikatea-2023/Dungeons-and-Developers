c04e090c3c9cdbdddab2c08dd4880df5e9a641a4
Jordan <jordanlowe659@gmail.com>
2023-06-16 12:28:47 +1200

    Added CSS styling and Div elements

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -5,15 +5,65 @@ import Form from './Form'
 import Game from './Game'
 
 function App() {
- 
-  
-
   return (
-    <main>
-      <Form />
-      <Game />
-
-    </main>
+    <>
+      <div id="container">
+        <div id="player">
+          <p>
+            Lorem Ipsum es simplemente el texto de relleno de las imprentas y
+            archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar
+            de las industrias desde el año 1500, cuando un impresor (N. del T.
+            persona que se dedica a la imprenta) desconocido usó una galería de
+            textos y los mezcló de tal manera que logró hacer un libro de textos
+            especimen. No sólo sobrevivió 500 años, sino que tambien ingresó
+            como texto de relleno en documentos electrónicos, quedando
+            esencialmente igual al original. Fue popularizado en los 60s con la
+            creación de las hojas "Letraset", las cuales contenian pasajes de
+            Lorem Ipsum, y más recientemente con software de autoedición, como
+            por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem
+            Ipsum.
+          </p>
+        </div>
+        <div id="scoreBoard">
+          <p>
+            Lorem Ipsum es simplemente el texto de relleno de las imprentas y
+            archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar
+            de las industrias desde el año 1500, cuando un impresor (N. del T.
+            persona que se dedica a la imprenta) desconocido usó una galería de
+            textos y los mezcló de tal manera que logró hacer un libro de textos
+            especimen. No sólo sobrevivió 500 años, sino que tambien ingresó
+            como texto de relleno en documentos electrónicos, quedando
+            esencialmente igual al original. Fue popularizado en los 60s con la
+            creación de las hojas "Letraset", las cuales contenian pasajes de
+            Lorem Ipsum, y más recientemente con software de autoedición, como
+            por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem
+            Ipsum.
+          </p>
+        </div>
+        <div id="center-content">
+          <h1>Dungeons & Developers</h1>
+          <div id="canvas">
+            <Game />
+          </div>
+          <div id="commentBox">
+            <p>
+              Lorem Ipsum es simplemente el texto de relleno de las imprentas y
+              archivos de texto. Lorem Ipsum ha sido el texto de relleno
+              estándar de las industrias desde el año 1500, cuando un impresor
+              (N. del T. persona que se dedica a la imprenta) desconocido usó
+              una galería de textos y los mezcló de tal manera que logró hacer
+              un libro de textos especimen. No sólo sobrevivió 500 años, sino
+              que tambien ingresó como texto de relleno en documentos
+              electrónicos, quedando esencialmente igual al original. Fue
+              popularizado en los 60s con la creación de las hojas "Letraset",
+              las cuales contenian pasajes de Lorem Ipsum, y más recientemente
+              con software de autoedición, como por ejemplo Aldus PageMaker, el
+              cual incluye versiones de Lorem Ipsum.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
