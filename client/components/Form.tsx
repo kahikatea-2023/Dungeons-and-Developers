5a34e009a7f38b506644c3d01aebf04696f72bb2
Jane Zhang <jiejanezhang2012@gmail.com>
2023-06-16 12:06:42 +1200

    done handleSelect function

--- client/components/Form.tsx
+++ client/components/Form.tsx
@@ -1,17 +1,18 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query"
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
 import React, { useEffect, useState } from "react"
 import { getClasses } from "../apis/api"
+import { User, UserDraft } from "../../models/models"
 
 
 function Form() {
 
   const [userData, setUserData] = useState({
-    playerName: '',
-    className: '',
-  })
+    name: '',
+    classId: 0,
+  } as UserDraft)
 
   // get the classes list// need some help
-  const { isLoading, playerClasses } = useQuery('getClasses', async () => {
+  const { isLoading, data } = useQuery(['getClasses'], async () => {
     return await getClasses()
   })
 
@@ -24,8 +25,11 @@ function Form() {
     setUserData(newUserData)
   }
 
-  function handleSelect(event: React.ReactEvent<HTMLSelectElement>) {
+  function handleSelect(event: React.ChangeEvent<HTMLSelectElement>) {
+    const name = event.target.name
+    const value = +event.target.value
 
+    const newUserData: UserDraft = { ...userData, classId: value }
   }
 
   function handleSubmit() {
@@ -37,16 +41,18 @@ function Form() {
       <form action="">
         <div>
           <label htmlFor="name">Player Name: </label>
-          <input type="text" name="name" id="" onChange={handleChange} value={userData.playerName} />
+          <input type="text" name="name" id="" onChange={handleChange} value={userData.name} />
         </div>
         <div>
           <label htmlFor="class">Player Class</label>
-          <select name="class" id="" onSelect={handleSelect} value={playerClassId}>
-            <option value="">Select your class</option>
-            {playerClasses.map((playerClass) =>
-              <option key={playerClassId} value={playerClassId}>{playerClass.name}</option>
-            )}
-          </select>
+          {!isLoading && data &&
+            <select name="classId" id="" onChange={handleSelect}>
+              <option value="">Select your class</option>
+              {data.map((playerClass) =>
+                <option key={playerClass.id} value={playerClass.id} >{playerClass.className}</option>
+              )}
+            </select>
+          }
         </div>
         <div>
           <button type="submit" onSubmit={handleSubmit}>Submit</button>
